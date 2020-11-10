d20c013c578adc7360c97b5250274a6111717d09
SgtMilk <alix.routhier-lalonde@hotmail.com>
2020-11-10 02:05:29 -0500

    update

--- peapod/src/subpages/Dashboard.js
+++ peapod/src/subpages/Dashboard.js
@@ -6,11 +6,28 @@
 
 import React from 'react'
 import './Dashboard.css'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { StylesProvider } from '@material-ui/core/styles';
+import yellow from '@material-ui/core/colors/yellow'
 
 export const Dashboard = () => {
     return (
         <div className = 'dashboard'>
-            <p>Dashboard</p>
+            <div className = 'titlebar-dashboard'>
+                <p>Peapod</p>
+            </div>
+            <div className = 'body-dashboard'>
+                <div className = 'wrapper-button-dashboard' id = 'wrapper-button-login'>
+                    <button class="grid-item-dashboard">
+                        <StylesProvider injectFirst>
+                            <CircularProgress variant="static" value={70} size = {'25vh'} color = {yellow['A100']}/>
+                        </StylesProvider>
+                    </button>
+                    <button class="grid-item-dashboard">2</button>
+                    <button class="grid-item-dashboard">3</button>
+                    <button class="grid-item-dashboard">4</button>
+                </div>
+            </div>
         </div>
     )
 }
