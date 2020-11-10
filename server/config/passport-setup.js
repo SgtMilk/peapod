03b18795ac4555c72a98bc3e950c96b2d8404acf
RiccLQL <liuricky24@gmail.com>
2020-11-10 02:23:48 -0500

    pgsql queries

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -31,20 +31,13 @@ passport.deserializeUser(async (id, done) => {
   const connection = await pool.connect();
   const userQuery = await connection.query(`SELECT * FROM ${tables.users} WHERE user_uuid='${id}'`);
   const user = userQuery.rows[0];
-
+  connection.release();
   if (!user) {
-    done(new Error("Failed to deserialize an user"));
+    done(new Error("Failed to deserialize an user."));
   }
   else {
-    done(null, user.rows[0]);
+    done(null, user);
   }
-  /*User.findById(id)
-    .then((user) => {
-      done(null, user);
-    })
-    .catch(() => {
-      done(new Error("Failed to deserialize an user"));
-    });*/
 });
 
 //  https://stackoverflow.com/questions/56798593/facebook-authentication-strategy-with-passport-js-express-and-typescript
@@ -59,24 +52,31 @@ passport.use(
       const connection = await pool.connect();
       const userQuery = await connection.query(`SELECT * FROM ${tables.users} WHERE google_id='${profile.id}' OR email='${profile.emails[0].value}';`);
       const user = userQuery.rows[0];
-      /*const user = await User.findOne({
-        $or: [{ googleID: profile.id }, { email: profile.emails[0].value }],
-      });*/
-
       if (!user) {
         /* 
         Create a user
         */
         const uuid = uuidv4();
-        user = await connection.query(`INSERT INTO ${tables.users}(user_uuid, email, firstname, lastname, google_id, facebook_id, riskLevel) VALUES('${uuid}', '${profile.email}', '${profile.firstname}', '${profile.lastname}', '${profile.googleID}', '${profile.facebookID}', '${profile.riskLevel}');`);
-        if (user.rows[0]) done(null, user);
+        const newUserQuery = await connection.query(`INSERT INTO ${tables.users}(user_uuid, email, firstname, lastname, google_id, facebook_id, riskLevel) VALUES('${uuid}', '${profile.email}', '${profile.firstname}', '${profile.lastname}', '${profile.googleID}', '${profile.facebookID}', '${profile.riskLevel}');`);
+        const newUser = newUserQuery.rows[0];
+        connection.release();
+        if (!newUser) {
+          done(new Error("Cannot create a new user!"));
+        } else {
+          done(null, newUser);
+        }
       } else if (user && !user.googleID) {
         /*
         Update user's google id
         */
-        user = await connection.query(`UPDATE ${tables.users} SET google_id='${profile.id}' WHERE email='${profile.emails[0].value}';`);
-      }
+        const updatedUserQuery = await connection.query(`UPDATE ${tables.users} SET google_id='${profile.id}' WHERE email='${profile.emails[0].value}';`);
+        const updatedUser = updatedUserQuery.rows[0];
+        connection.release();
+        done(null, updatedUser);
+      } else {
+        connection.release();
         done(null, user);
+      }
     }
   )
 );
