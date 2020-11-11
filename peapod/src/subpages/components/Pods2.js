0b3d530999854a6d701fa22337698c87634301f1
AdamMigliore <adam.dire@gmail.com>
2020-11-10 23:17:24 -0500

    fixed expansion bug

--- peapod/src/subpages/components/Pods2.js
+++ peapod/src/subpages/components/Pods2.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import './Pods2.css'
+import React from "react";
+import "./Pods2.css";
 import redux from "../../index";
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export const Pods2 = (index) => {
+  const [isExpanded, setIsExpanded] = React.useState(false);
 
-    const expand = (index) => {
-        console.log('beep')
-        if(document.getElementById('pods2').style.height === '3vh')document.getElementById('pods2').style.height = '20vh';
-        else document.getElementById('pods2').style.height = '3vh';
-    }
-
-    return (
-        <button className = 'pods2' id = 'pods2' onClick = {expand}>
-            <p className = 'groupname-pods2'>{redux.store.getState().pods[index.props].groupname}</p>
-            <div className = 'wrapper-progress-pods2'>
-                <CircularProgress variant="static" value={redux.store.getState().pods[index.props].maxValue} size = {'5vh'}/>
-                <p className = 'percentage-pods2'>{`${redux.store.getState().pods[index.props].maxValue}%`}</p>
-            </div>
-        </button>
-    )
-}
+  const expand = (index) => {
+    console.log("beep");
+    setIsExpanded(!isExpanded);
+  };
+  const expandMe = isExpanded ? "pods2 expanded" : "pods2";
+  return (
+    <button className={expandMe} id="pods2" onClick={expand}>
+      <p className="groupname-pods2">
+        {redux.store.getState().pods[index.props].groupname}
+      </p>
+      <div className="wrapper-progress-pods2">
+        <CircularProgress
+          variant="static"
+          value={redux.store.getState().pods[index.props].maxValue}
+          size={"5vh"}
+        />
+        <p className="percentage-pods2">{`${
+          redux.store.getState().pods[index.props].maxValue
+        }%`}</p>
+      </div>
+    </button>
+  );
+};
