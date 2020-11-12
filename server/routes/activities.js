f53e30a953797bb61d351ff5e5a6c2d8957a5d88
RiccLQL <liuricky24@gmail.com>
2020-11-12 00:55:17 -0500

    fixed id

--- server/routes/activities.js
+++ server/routes/activities.js
@@ -12,10 +12,10 @@ const { authCheck } = require("../controllers/auth");
 /* POST an activity */
 activitiesRouter.post("/", authCheck, postActivity);
 /* GET an activity by uuid */
-activitiesRouter.get("/", authCheck, getActivity);
+activitiesRouter.get("/:id", authCheck, getActivity);
 /* GET all activities for a user or get 3 activities from the user, sorted by date created */
 activitiesRouter.get("/", authCheck, getActivities);
 /* DELETE an activity */
-activitiesRouter.delete("/", authCheck, deleteActivity);
+activitiesRouter.delete("/:id", authCheck, deleteActivity);
 
 module.exports = activitiesRouter;
